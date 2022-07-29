bb5f1a1da923c48c876342160ad9b51bab13e9d7
C836 <gl.sepiol@gmail.com>
2022-07-29 00:47:11 -0300

    Implementando sessão de pesquisa da página

---
+++ src/App.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Input_Wrapper = styled.section`
+  width: 600px;
+  max-width: 90%;
+
+  position: absolute;
+  top: 50%; left: 50%;
+  translate: -50% -50%;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import * as S from './App.styled'
+import { useState } from 'react';
 import 'ress'
+import Input from './components/Input/Input'
+import { Headline } from './components/Text/Text.styled';
+import { Properties_Config } from './services/search.config';
 import GlobalStyled from './global.styled';
 
 function App() {
+  const [location_list, setList] = useState<Properties_Config[] | undefined>();
+
   return (
     <div>
       <GlobalStyled />
+
+      <S.Input_Wrapper>
+        <Headline>
+          Como está o tempo hoje?
+        </Headline>
+        <Input setList={setList} />
+      </S.Input_Wrapper>
     </div>
   )
 }
