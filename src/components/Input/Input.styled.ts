1a06c5c7f6631e8372f283d15376f2e6f0829e5c
C836 <gl.sepiol@gmail.com>
2022-07-29 00:44:31 -0300

    Estilizando componente Input

--- src/components/Input/Input.styled.ts
+++ src/components/Input/Input.styled.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
 export const Input = styled.input`
-    
+  width: 100%;
+  padding: 3%;
+
+  background-color: white;
+  opacity: 75%;
+  border-radius: 10px;
+  outline: none;
+
+  font-family: 'Montserrat', sans-serif;
+  font-size: clamp(1rem, 5vw, 1.25rem);
+  color: #6AA2D1;
 `
