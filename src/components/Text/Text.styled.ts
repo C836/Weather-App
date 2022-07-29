d63ba70908f707408bafec4ea17ce82d8a7623f3
C836 <gl.sepiol@gmail.com>
2022-07-29 00:44:43 -0300

    Implementando componente Headline

---
+++ src/components/Text/Text.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Headline = styled.h1`
+  width: 100%;
+
+  font-family: 'Montserrat', sans-serif;
+  font-size: clamp(1rem, 6.7vw, 2.8rem);
+  text-align: center;
+  color: white;
+`
