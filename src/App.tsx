400ff4ba83c20a19a22ad3b14121c67fd7fdf69e
C836 <gl.sepiol@gmail.com>
2022-07-30 10:36:13 -0300

    Implementando interface com informações do clima

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,27 @@ import { useEffect, useState } from 'react';
 import 'ress'
 import Input from './components/Input/Input'
 import Select from './components/Select/Select';
-import { Headline } from './components/Text/Text.styled';
+import { Headline, Paragraph } from './components/Text/Text.styled';
 import { Properties_Config } from './services/search.config';
 import GlobalStyled from './global.styled';
+import { weather_request } from './services/weather';
+import { Weather_Config } from './services/weather.config';
 
 function App() {
   const [location_list, setList] = useState<Properties_Config[] | undefined>();
-  const [location_data, setLocation] = useState({search: "", data:{}})
+  const [location_data, setLocation] = useState<{search: string, data: Weather_Config | undefined}>({search: "", data: undefined})
+  const { search, data } = location_data
 
   useEffect(() => {
-    console.log(location_data)
-  },[location_data])
+    weather_request(location_data.search)
+    .then(response => setLocation({...location_data, data: response}));
+  },[location_data.search])
 
   return (
     <div>
       <GlobalStyled />
 
-      <S.Home>
+      <S.Home disabled={location_data.search ? true : false}>
         <S.Input_Wrapper>
           <Headline>
             Como está o tempo hoje?
@@ -29,10 +33,34 @@ function App() {
             setList={setList} />
           <Select 
             location_list={location_list}
+            setList={setList}
             location_data={location_data}
             setLocation={setLocation} />
         </S.Input_Wrapper>
       </S.Home>
+
+      <S.Weather disabled={location_data.search ? false : true}>
+        <Headline>
+          {search.toUpperCase()}
+        </Headline>
+
+        <Paragraph>
+          {data?.weather[0].description}
+        </Paragraph>
+
+        <S.Weather_Wrapper>
+          <p>{data?.main.temp}</p>
+          <img src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}/>
+        </S.Weather_Wrapper>
+
+        <Paragraph>
+          <b>MAX: </b>{data?.main.temp_max} <b>MIN: </b>{data?.main.temp_min}
+        </Paragraph>
+        
+        <Paragraph>
+          Ver previsão para os próximos 5 dias
+        </Paragraph>
+      </S.Weather>
     </div>
   )
 }
--- src/components/Select/Select.config.ts
+++ src/components/Select/Select.config.ts
@@ -2,6 +2,7 @@ import { Properties_Config } from "../../services/search.config";
 
 export interface Select_Config {
   location_list: Properties_Config[] | undefined;
+  setList: React.Dispatch<React.SetStateAction<Properties_Config[] | undefined>>;
   location_data: location_data;
   setLocation: React.Dispatch<React.SetStateAction<location_data>>;
 }
