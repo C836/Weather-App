29dfee8c75dfda7756426309544d4abb2cae83da
C836 <gl.sepiol@gmail.com>
2022-07-30 12:31:12 -0300

    Estilizando e formatando display de clima principal

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -14,3 +14,23 @@ export const Input_Wrapper = styled.section`
   flex-direction: column;
   row-gap: 16px;
 `
+
+export const Weather = styled.div<{disabled: boolean}>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  position: absolute;
+  top: 50%; left: 50%;
+  translate: -50% -50%;
+
+  transition: opacity 0.3s;
+
+  opacity: ${props => props.disabled && 0};
+  pointer-events: ${props => props.disabled && "none"};
+`
+
+export const Weather_Wrapper = styled.div`
+  display: flex ;
+  align-items: center;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { useEffect, useState } from 'react';
 import 'ress'
 import Input from './components/Input/Input'
 import Select from './components/Select/Select';
-import { Headline, Paragraph } from './components/Text/Text.styled';
+import { Anchor, Headline, Paragraph } from './components/Text/Text.styled';
 import { Properties_Config } from './services/search.config';
 import background from "./assets/img/background.jpg"
 import GlobalStyled from './global.styled';
 import { weather_request } from './services/weather';
 import { Weather_Config } from './services/weather.config';
+
+import { capitalize } from './utils/capitalize'
+import { getDegrees } from './utils/getDegrees';
 
 function App() {
   const [location_list, setList] = useState<Properties_Config[] | undefined>();
@@ -24,7 +27,7 @@ function App() {
     <div>
       <GlobalStyled background={background} />
 
-      <S.Home disabled={location_data.search ? true : false}>
+      <S.Home>
         <S.Input_Wrapper>
           <Headline>
             Como está o tempo hoje?
@@ -45,21 +48,22 @@ function App() {
           {search.toUpperCase()}
         </Headline>
 
-        <Paragraph>
-          {data?.weather[0].description}
+        <Paragraph size={"1.2rem"}>
+          {data && capitalize(data.weather[0].description)}
         </Paragraph>
 
         <S.Weather_Wrapper>
-          <p>{data?.main.temp}</p>
+          <Paragraph size={"4rem"}>{data && getDegrees(data?.main.temp)}</Paragraph>
           <img src={`http://openweathermap.org/img/wn/${data?.weather[0].icon}@2x.png`}/>
         </S.Weather_Wrapper>
 
-        <Paragraph>
-          <b>MAX: </b>{data?.main.temp_max} <b>MIN: </b>{data?.main.temp_min}
+        <Paragraph size={"1.2rem"}>
+          <b>MAX: </b>{data && getDegrees(data?.main.temp_max)} 
+          <b>MIN: </b>{data && getDegrees(data?.main.temp_min)}
         </Paragraph>
         
-        <Paragraph>
-          Ver previsão para os próximos 5 dias
+        <Paragraph size={".75rem"} lineHeight={5}>
+          <Anchor>Ver previsão para os próximos 5 dias</Anchor>
         </Paragraph>
       </S.Weather>
     </div>
