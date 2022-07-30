433bf6b181848c9f3c934a7dc92550e1bd0ddde1
C836 <gl.sepiol@gmail.com>
2022-07-30 15:41:47 -0300

    Implementando método de retorno e formatação dos dias da semana

---
+++ src/utils/getDate.ts
@@ -0,0 +1,13 @@
+export function get_date(date: string) {
+  const week = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+  const month = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+  const today = new Date(date);
+  const tomorrow = new Date(today.setDate(today.getDate() + 1));
+  
+  const current_day = week[tomorrow.getDay()];
+  const current_day_number = String(tomorrow.getDate()).padStart(2, "0");
+  const current_month = month[tomorrow.getMonth()];
+
+  return `${current_day}, ${current_day_number} ${current_month}`
+}
