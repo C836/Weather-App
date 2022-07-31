72ed3fd4fd3a34339671c3e28712cc5e0373e989
C836 <gl.sepiol@gmail.com>
2022-07-31 00:16:34 -0300

    Transferindo requisição da API para o componente Select

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,9 @@
+import { useEffect } from "react";
+
 import * as S from "./Select.styled";
 import { Select_Config } from "./Select.config";
+
+import { weather_request } from "../../services/weather";
 import { Properties_Config } from "../../services/search.config";
 
 export default function Select({
@@ -8,12 +12,18 @@ export default function Select({
   locationData,
   setLocation,
 }: Select_Config) {
+
   const Select_Option = (event: React.MouseEvent<HTMLButtonElement>) => {
     const selectedLocation = event.currentTarget.value;
 
     setLocation({ ...locationData, search: selectedLocation! });
     setList([]);
   };
+
+  useEffect(() => {
+    weather_request(locationData.search)
+    .then(response => setLocation({...locationData, data: response}));
+  },[locationData.search])
 
   return (
     <S.Select>
