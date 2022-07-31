53db1edcf730c8c258df46795b053de7b811a140
C836 <gl.sepiol@gmail.com>
2022-07-31 15:31:30 -0300

    Implementando arquivo Index ao diretório assets

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import en from "./img/flags/en.png"
+import es from "./img/flags/es.png"
+import pt from "./img/flags/pt.png"
+
+import background from "./img/background.jpg"
+
+export { en, es, pt, background }
