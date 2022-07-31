fed130d30bddd95fc400965c17c7516d40dd63d7
C836 <gl.sepiol@gmail.com>
2022-07-31 15:49:38 -0300

    Refatorando componentes de Texto

--- src/components/Text/Text.styled.ts
+++ src/components/Text/Text.styled.ts
@@ -1,22 +1,28 @@
 import styled from "styled-components";
 
+interface ParagraphConfig {
+  size?: string;
+  align?: string;
+  lineHeight?: number;
+}
+
 export const Headline = styled.h1`
   width: 100%;
 
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-size: clamp(1rem, 6.7vw, 2.8rem);
   text-align: center;
   color: white;
-`
+`;
 
-export const Paragraph = styled.p<{size?: string, align?: string, lineHeight?: number}>`
-  font-family: 'Montserrat', sans-serif;
+export const Paragraph = styled.p<ParagraphConfig>`
+  font-family: "Montserrat", sans-serif;
   color: white;
 
-  font-size: ${props => props.size || "1rem"};
-  text-align: ${props => props.align || "left"};
-  line-height: ${props => props.lineHeight || 1};
-`
+  font-size: ${(props) => props.size || "1rem"};
+  text-align: ${(props) => props.align || "left"};
+  line-height: ${(props) => props.lineHeight || 1};
+`;
 
 export const Anchor = styled.a`
   cursor: pointer;
@@ -24,4 +30,4 @@ export const Anchor = styled.a`
   :hover {
     text-decoration: underline;
   }
-`
+`;
