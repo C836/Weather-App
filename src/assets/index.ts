590a6b90507a5abe9dadc2c4e60f23bd3c486766
C836 <gl.sepiol@gmail.com>
2022-07-31 15:41:57 -0300

    Implementando arquivo de traduções

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,6 @@ import es from "./img/flags/es.png"
 import pt from "./img/flags/pt.png"
 
 import background from "./img/background.jpg"
+import translations from "./text/translations.json"
 
-export { en, es, pt, background }
+export { en, es, pt, background, translations }
